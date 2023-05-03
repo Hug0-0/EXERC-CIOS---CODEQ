a2f22b4d0852c64fa702831c0454459ecc07df5f
Hugo Campos <132182230+Hug0-0@users.noreply.github.com>
2023-05-03 08:59:30 -0300

    Add files via upload

---
+++ Questão 15.py
@@ -0,0 +1,6 @@
+x=int(input("valor do primeiro botão pressionado x:"))
+y=int(input("valor do segundo botão pressionado y:"))
+C=x+y
+
+musicas = ['PROXYCITY','P.Y.N.G.','DNSUEY!','SERVER','HOST!','CRIPTONIZE','OFFLINE DAY','SALT','ANSWER','RAR?','WIFI ANTENNAS']
+print(musicas[C])
